6f3ec4c31773b448b5008ecd6d40352a8ff779ce
cuong0209992 <sandtimezone92@gmail.com>
2021-03-09 09:51:47 +0700

    thực hành về biến

---
+++ nhap-bien.js
@@ -0,0 +1,10 @@
+let a = prompt("nhập giá trị a vào: ");
+let b = prompt("nhập giá trị b vào: ");
+
+
+if (a % b == 0) {
+    alert("b là bội số của a");
+} else {
+    alert("b không phải là bội số của a");
+}
+
---
+++ show-dien-tich.js
@@ -0,0 +1,5 @@
+let rong = 10;
+let dai = 30;
+let dt = dai * rong;
+
+document.write("diện tích hình chữ nhật là: "+dt);
